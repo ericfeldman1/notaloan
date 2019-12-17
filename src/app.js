f82cc971183004b52718e3e40394b6939b171803
ericfeldman1 <ericfeldman2@gmail.com>
2019-12-16 20:34:16 -0500

    updates

--- src/app.js
+++ src/app.js
@@ -4,30 +4,38 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const careersRouter = require('./careers/careers-router')
+const loansRouter = require('./loans/loans-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
+app.use(helmet())
+
+app.use('/api/careers', careersRouter)
+app.use('/api/loans', loansRouter)
+
+app.get('/xss', (req, res) => {
+  res.cookie('secretToken', '1234567890');
+  res.sendFile(__dirname + '/xss-example.html');
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-    } else {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
     console.error(error)
     response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-    })
+  }
+  res.status(500).json(response)
+})
 
 module.exports = app
